35306d1007f33f533aefbb88dd13f91aa39cbd9d
zhuliangbin <825685183@qq.com>
2023-10-24 11:34:53 +0800

    遗漏提交

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
 const serverConfig = {
-  baseURL: "/api", // 请求基础地址,可根据环境自定义 http://www.lxce.net:8080/
+  baseURL: "http://www.lxce.net/r", // 请求基础地址,可根据环境自定义 http://www.lxce.net/
   useTokenAuthorization: false, // 是否开启 token 认证
 };
 export default serverConfig;
